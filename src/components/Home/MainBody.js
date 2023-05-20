38c1cc358abeaa4c06fc4edc0e1521263475e5bc
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2023-05-20 18:14:36 +0530

    Add style for cards

--- src/components/Home/MainBody.js
+++ src/components/Home/MainBody.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import Card from "../card/Card";
 import { Link } from "react-router-dom";
 import ShimmerCard from "../UI/ShimmerCard";
-import "./MainBody.css";
-import "../../../index.css";
 import LostConnection from "../pages/LostConnection";
 import useGetRestaurant from "../../hooks/useGetRestaurant";
 import { filterRestaurants } from "../utils/helper";
@@ -11,14 +9,13 @@ import useIsOnline from "../../hooks/useIsOnline";
 
 const MainBody = () => {
 	const [searchText, setSearchText] = useState("");
-	const getRestaurants = useGetRestaurant()
+	const getRestaurants = useGetRestaurant();
 	const [filterRestaurant, setFilterRestaurant] = useState([]);
 	const isOnline = useIsOnline();
 
 	useEffect(() => {
-		setFilterRestaurant(getRestaurants)
-	}, [getRestaurants])
-
+		setFilterRestaurant(getRestaurants);
+	}, [getRestaurants]);
 
 	if (!isOnline) {
 		return <LostConnection />;
@@ -26,22 +23,27 @@ const MainBody = () => {
 
 	return (
 		<div className="body">
-			<div className="search-form">
+			<div className="flex justify-center my-4">
 				<input
 					type="text"
 					placeholder="Search..."
 					onChange={(e) => setSearchText(e.target.value)}
+					className="h-10 bottom-2 w-96 px-4 py-4 outline-none"
 				/>
-				<button type="button" onClick={() => {
-					const filter = filterRestaurants(getRestaurants, searchText)
-					setFilterRestaurant(filter);
-				}}>
+				<button
+					type="button"
+					className="h-10 w-20 bg-zinc-900 text-white"
+					onClick={() => {
+						const filter = filterRestaurants(getRestaurants, searchText);
+						setFilterRestaurant(filter);
+					}}
+				>
 					Search
 				</button>
 			</div>
 
 			{filterRestaurant?.length === 0 && <ShimmerCard />}
-			<div className="cards">
+			<div className="flex flex-wrap gap-6 justify-center mx-4 overflow-x-hidden">
 				{filterRestaurant?.map((restaurant) => (
 					<Link
 						to={"/card-detail/" + restaurant.data.id}
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,16 +4,16 @@ const Card = (props) => {
 	const { avgRating, cuisines, name, cloudinaryImageId } = props.resData.data;
 
 	return (
-		<div className="card">
-			<div className="card__img">
-				<img src={CARD_IMG + cloudinaryImageId} />
+		<div className="w-72 h-auto rounded-lg bg-white shadow-md p-4">
+			<div className="">
+				<img src={CARD_IMG + cloudinaryImageId} className="w-auto h-auto object-cover" />
 			</div>
 
-			<h2 className="card_hed">{name}</h2>
+			<h2 className="">{name}</h2>
 
-			<p className="cousines">{cuisines.join(",")}</p>
+			<p className="whitespace-normal overflow-hidden">{cuisines.join(",")}</p>
 
-			<div className="star_rating">
+			<div className="">
 				<p> {avgRating} ⭐</p>
 			</div>
 		</div>
